9a0e424dfc38e888efe5e175868c048a4ec80d85
coder-sadhin <codersadhin@gmail.com>
2024-08-27 18:55:12 +0600

    input tree

--- insert_in_bst.cpp
+++ insert_in_bst.cpp
@@ -13,7 +13,49 @@ public:
         this->right = NULL;
     }
 };
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
 
+        // 2. jabotiyo ja kaj ache
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. children gulo ke push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
 
 
 int main()
