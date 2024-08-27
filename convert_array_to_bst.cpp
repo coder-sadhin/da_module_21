f69f8e87b666e4843dfb50241b2dab08a77665aa
coder-sadhin <codersadhin@gmail.com>
2024-08-27 18:51:16 +0600

    array to input

--- convert_array_to_bst.cpp
+++ convert_array_to_bst.cpp
@@ -19,5 +19,9 @@ int main()
     int n;
     cin >> n;
     int a[n];
-    
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    Node *root = convert(a, n, 0, n - 1);
+    level_order(root);
+    return 0;
 }
